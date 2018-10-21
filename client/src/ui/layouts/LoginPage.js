6254257d312c17ebd7594388f0e60145525a4238
zahra falah <sepidehfallah2@gmail.com>
2018-10-20 19:44:07 -0700

    Authentication

--- client/src/ui/layouts/LoginPage.js
+++ client/src/ui/layouts/LoginPage.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
 
-import CRUD from '../../utils/api/CRUD';
+import axios from 'axios';
 
 class LoginForm extends React.Component {
-  state = {
-    username: '',
-    password: ''
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+   
+  } 
+
+  handleClick(){
+    axios.post(`/authenticate/admin`, { firstname: "", password: "" })
+    .then(function(response){
+      console.log('saved successfully')
+    });  
+  }
+
+  handleChange (event){
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
 
   render() {
+    console.log(this.state)
+
     return (
       <div className="login-form">
         <style>{`
@@ -26,16 +49,24 @@ class LoginForm extends React.Component {
             </Header>
             <Form size="large">
               <Segment stacked>
-                <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail address" />
+                <Form.Input 
+                  fluid 
+                  icon="user" 
+                  iconPosition="left" 
+                  placeholder="E-mail address"
+                  type="text"  
+                  name="username" value={this.state.username} onChange={this.handleChange} 
+                  />
                 <Form.Input
                   fluid
                   icon="lock"
                   iconPosition="left"
                   placeholder="Password"
                   type="password"
+                  name="password" value={this.state.password} onChange={this.handleChange} 
                 />
 
-                <Button color="teal" fluid size="large">
+                <Button color="teal" fluid size="large" onClick={(e) => this.handleClick(e)}>
                   Login
                 </Button>
               </Segment>
