cb38bd94b9c7a1df7747d59a7de6eeffa32ab370
Trevor Johnson <johnsontrevor55@gmail.com>
2018-10-21 10:40:06 -0700

    home layout

--- client/src/ui/layouts/HomePage.js
+++ client/src/ui/layouts/HomePage.js
@@ -3,20 +3,18 @@ import React, { Component } from 'react';
 import {
   Button,
   Container,
-  Divider,
   Grid,
   Header,
   Icon,
-  Image,
   List,
   Menu,
   Responsive,
   Segment,
   Sidebar,
   Visibility,
-  Form, TextArea
+  Form,
+  TextArea
 } from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
 
 class HomepageHeading extends Component {
   state = { selectedFile: null };
@@ -94,13 +92,15 @@ class DesktopContainer extends Component {
               size="large"
             >
               <Container>
-                <Menu.Item as="a"></Menu.Item>
-                <Menu.Item as="a"></Menu.Item>
-                <Menu.Item as="a"></Menu.Item>
-                <Menu.Item as="a"></Menu.Item>
+                <Menu.Item as="a" />
+                <Menu.Item as="a" />
+                <Menu.Item as="a" />
+                <Menu.Item as="a" />
                 <Menu.Item position="right">
-                  <Button  inverted style={{ marginLeft: '0.5em' }}>
-                  <a href="https://www.childrenscancernetwork.org/" target="_blank">Website</a>
+                  <Button inverted style={{ marginLeft: '0.5em' }}>
+                    <a href="https://www.childrenscancernetwork.org/" target="_blank">
+                      Website
+                    </a>
                   </Button>
                   <Button as="a" inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
                     Log Out
@@ -141,8 +141,7 @@ class MobileContainer extends Component {
       <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
         <Sidebar.Pushable>
           <Sidebar as={Menu} animation="uncover" inverted vertical visible={sidebarOpened}>
-            <Menu.Item as="a" active>
-            </Menu.Item>
+            <Menu.Item as="a" active />
             <Menu.Item as="a">Log Out</Menu.Item>
           </Sidebar>
 
@@ -162,11 +161,7 @@ class MobileContainer extends Component {
                   <Menu.Item onClick={this.handleToggle}>
                     <Icon name="sidebar" />
                   </Menu.Item>
-                  <Menu.Item position="right">
-                    {/* <Button as="a" inverted style={{ marginLeft: '0.5em' }}>
-                      Log Out
-                    </Button> */}
-                  </Menu.Item>
+                  <Menu.Item position="right" />
                 </Menu>
               </Container>
               <HomepageHeading mobile />
@@ -200,25 +195,13 @@ const HomepageLayout = () => (
     <Segment style={{ padding: '8em 0em' }} vertical>
       <Grid container stackable verticalAlign="middle">
         <Grid.Row>
-          {/* <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              We Help Companies and Companions
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              We can give your company superpowers to do things that they never thought possible.
-              Let us delight your customers and empower your needs... through pure data analytics.
-            </p>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              We Make Bananas That Can Dance
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes that's right, you thought it was the stuff of dreams, but even bananas can be
-              bioengineered.
-            </p>
-          </Grid.Column> */}
           <Grid.Column floated="right">
             <Form>
-              <TextArea autoHeight placeholder='Try adding multiple lines' style={{ minHeight: 100 }} />
+              <TextArea
+                autoHeight
+                placeholder="Try adding multiple lines"
+                style={{ minHeight: 100 }}
+              />
             </Form>
           </Grid.Column>
         </Grid.Row>
@@ -229,8 +212,7 @@ const HomepageLayout = () => (
         </Grid.Row>
       </Grid>
     </Segment>
- 
- 
+
     <Segment inverted vertical style={{ padding: '5em 0em' }}>
       <Container>
         <Grid divided inverted stackable>
@@ -238,9 +220,21 @@ const HomepageLayout = () => (
             <Grid.Column width={3}>
               <Header inverted as="h4" content="About" />
               <List link inverted>
-                <List.Item ><a href="https://www.childrenscancernetwork.org/" target="_blank">Website</a></List.Item>
-                <List.Item ><a href="https://www.salesforce.com/" target="_blank">Salesforce</a></List.Item>             
-                <List.Item ><a href="https://github.com/2018-Arizona-Opportunity-Hack/Team9" target="_blank">Work Source</a></List.Item>         
+                <List.Item>
+                  <a href="https://www.childrenscancernetwork.org/" target="_blank">
+                    Website
+                  </a>
+                </List.Item>
+                <List.Item>
+                  <a href="https://www.salesforce.com/" target="_blank">
+                    Salesforce
+                  </a>
+                </List.Item>
+                <List.Item>
+                  <a href="https://github.com/2018-Arizona-Opportunity-Hack/Team9" target="_blank">
+                    Work Source
+                  </a>
+                </List.Item>
               </List>
             </Grid.Column>
             <Grid.Column width={3}>
@@ -251,8 +245,6 @@ const HomepageLayout = () => (
                 <List.Item as="">Mohammad</List.Item>
                 <List.Item as="">Badri</List.Item>
                 <List.Item as="">Jonnathan</List.Item>
-                
-               
               </List>
             </Grid.Column>
           </Grid.Row>
